cc7b6d992ebd210e80874baf0206105d68cf1bfe
zappled <j0el_93@hotmail.com>
2022-10-24 15:39:16 +0800

    Added more components to 'volunteer requirements' page

--- client/src/pages/volunteer/VolunteerRequirements.js
+++ client/src/pages/volunteer/VolunteerRequirements.js
@@ -1,7 +1,8 @@
 import React from "react";
 import styles from "../../components/volunteer/VolunteerWithUs.module.css";
 import Carousel from "react-multi-carousel";
-import bannerTwo from "../../assets/volunteer_pages/volunteer_02.png";
+import mentorHowTo from "../../assets/volunteer_pages/mentor_01_howto.png";
+import bannerTwo from "../../assets/volunteer_pages/mentor_02_fundraising.jpeg";
 import bannerThree from "../../assets/volunteer_pages/volunteer_03.jpeg";
 import bannerFour from "../../assets/volunteer_pages/volunteer_04.png";
 import bannerFive from "../../assets/volunteer_pages/volunteer_05.jpeg";
@@ -39,6 +40,13 @@ const VolunteerRequirements = () => {
         </div>
       </div>
 
+      <div className={styles.coursegoals_container} style={{ height: "24vw" }}>
+        <h2>How do I become a mentor?</h2>
+        <div className={styles.goals_box} style={{ marginTop: "3vh" }}>
+          <img src={mentorHowTo} alt="" className="mentor_image" />
+        </div>
+      </div>
+
       {/* carousel to scroll through various mentor roles */}
       <div className={styles.carousel_container}>
         <span className={styles.related_courses}>Available Roles</span>
@@ -52,7 +60,7 @@ const VolunteerRequirements = () => {
           <div className={styles.content}>
             <div className={styles.content_label}>
               {/* first carousel item label */}
-              <span>Mentor (Module 1, 2 or 3)</span>
+              <span>Fundraising Volunteer</span>
             </div>
             {/* first carousel item image */}
             <div className={styles.content_image}>
@@ -66,35 +74,37 @@ const VolunteerRequirements = () => {
                   <span className={styles.roles_icons_clock}></span>
                   <span className={styles.roles_label}>Time Commitment</span>
                 </div>
-                <p>• Minimum period of 6 months</p>
-                <p style={{ marginLeft: "7.5vw" }}>
-                  • 1.5 hours every Sunday <b>OR</b>
-                </p>
-                <p style={{ marginLeft: "7.5vw" }}>• 3 hours every 2 Sundays</p>
+                <p>• Flexible</p>
               </div>
               {/* container sets icons in-line with label */}
               <div className={styles.roles_icons_container}>
                 <span className={styles.roles_icons_location}></span>
                 <span className={styles.roles_label}>Location</span>
               </div>
-              <p>
-                • Online on Zoom <b>OR</b>
-              </p>
-              <p>• FAST Hub @ 3 Chin Cheng Avenue, S429401</p>
+              <p>• Remote via Zoom</p>
               {/* container sets icons in-line with label */}
               <div className={styles.roles_icons_container}>
                 <span className={styles.roles_icons_requirements}></span>
                 <span className={styles.roles_label}>Requirements</span>
               </div>
-              <p>• Minimum 3 years of work experience in any field</p>
+              <p>
+                • Experience with Events Management or Business Development
+                preferred
+              </p>
               {/* container sets icons in-line with label */}
               <div className={styles.roles_icons_container}>
                 <span className={styles.roles_icons_responsibilities}></span>
                 <span className={styles.roles_label}>Responsibilities</span>
               </div>
-              <p>
-                • Adopt a class and take the students on a learning journey that
-                will build their confidence in themselves and their future
+              <p>• Any of the following:</p>
+              <p style={{ marginLeft: "7.5vw" }}>
+                • Help us organise our annual fundraising events
+              </p>
+              <p style={{ marginLeft: "7.5vw" }}>
+                • Propose your own fundraising initiative
+              </p>
+              <p style={{ marginLeft: "7.5vw" }}>
+                • Connect us with potential fundraising partners
               </p>
               {/* first carousel buttons */}
               <div className={styles.roles_button_container}>
