7b51bb76ba5654fd24b3a6db570a361b131a63a7
zappled <j0el_93@hotmail.com>
2022-10-24 14:46:52 +0800

    Set up 'volunteer requirements' page and route, removed header from homepage

---
+++ client/src/pages/volunteer/VolunteerRequirements.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const VolunteerRequirements = () => {
+  return (
+    <>
+      <div className="page_container">
+        {/* page_header shows current page navigation */}
+        <div className="page_header">
+          <span>
+            Get Involved / Volunteer / Available Roles / Mentor (Module 1, 2 or
+            3) Details
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default VolunteerRequirements;
